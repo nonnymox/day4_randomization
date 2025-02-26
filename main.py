d5a21b63452ec1868b8814488540620f78189d79
nonnymox <knonxho@gmail.com>
2025-02-26 05:50:47 -0800

    Created the heads or tails algorithm

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+# Generating random number
+# random_number = random.randint(1,10)
+# print(random_number)
+
+# Generating random floating point numbers
+# num = random.random()
+# print(num)
+
+# Heads or Tails Random Algorithm
+num = random.randint(0,1)
+if num == 0:
+    print("Heads")
+else:
+    print("Tails")
